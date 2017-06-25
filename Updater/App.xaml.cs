e31265c47793cf2ea7b09661788c8ed0fe82ecd9
하재현 <suneria@naver.com>
2017-06-25 22:25:14 +0900

    기본 클래스 추가

--- Updater/App.xaml.cs
+++ Updater/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Updater.Views;
 
 namespace Updater
 {
@@ -13,5 +14,13 @@ namespace Updater
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow window = new MainWindow();
+            MainViewModel mainViewModel = new MainViewModel();
+            window.DataContext = mainViewModel;
+            window.Show();
+        }
     }
 }
---
+++ Updater/Views/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Updater.Views
+{
+    class MainViewModel : BindableBase
+    {
+        public ICommand UpdateCommand
+        {
+            get
+            {
+                DelegateCommand command = new DelegateCommand(() => { });
+                return command;
+            }
+            set
+            {
+            }
+        }
+    }
+}
