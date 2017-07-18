62e8f4932a368ba2bfaef1984a428b704fb26f1c
하재현 <suneria@naver.com>
2017-07-18 23:37:51 +0900

    String Extension 추가

---
+++ Common/Extensions/StringExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Common.Extensions
+{
+    public static class StringExtension
+    {
+        public static int ToInt(this string numberInString)
+        {
+            return int.Parse(numberInString);
+        }
+
+        public static long ToLong(this string numberInString)
+        {
+            return long.Parse(numberInString);
+        }
+
+        public static float ToFloat(this string numberInString)
+        {
+            return float.Parse(numberInString);
+        }
+
+        public static DateTime ToDateTime(this string timeInString, string format)
+        {
+            if (!string.IsNullOrEmpty(timeInString))
+                return DateTime.ParseExact(timeInString, format, CultureInfo.InvariantCulture);
+            else
+                return DateTime.MinValue;
+        }
+    }
+}
